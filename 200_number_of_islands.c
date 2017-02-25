2ce2eb34bba95185f0a1e23eac32f73fbae27318
leonard520 <xiaoydin@cisco.com>
2017-02-25 23:22:34 +0800

    tree practise

--- 200_number_of_islands.c
+++ 200_number_of_islands.c
@@ -7,7 +7,7 @@
 #include <math.h>
 
 void numIslandsInternal(char** grid, int i, int j, int m, int n){
-  if(i < 0 || j < 0 || i >= m || j >= n || grid == '0'){
+  if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0'){
     return;
   }
   grid[i][j] = '0';
@@ -21,5 +21,14 @@ int numIslands(char** grid, int gridRowSize, int gridColSize) {
   if(grid == NULL){
     return 0;
   }
-
+  int n = 0;
+  for(int i = 0; i < gridRowSize; i++){
+    for(int j = 0; j < gridColSize; j++){
+      if(grid[i][j] == '1'){
+        numIslandsInternal(grid, i, j, gridRowSize, gridColSize);
+        n++;
+      }
+    }
+  }
+  return n;
 }
