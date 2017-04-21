6165fe80f4ba6f4008c575843a8ca43047d94580
leonard520 <xiaoydin@cisco.com>
2017-04-22 00:53:40 +0800

    be careful with each problem

--- 606_heaters.c
+++ 606_heaters.c
@@ -7,6 +7,7 @@
 #include <math.h>
 
 #define MAX(x,y) (x) > (y) ? (x) : (y)
+#define MIN(x,y) (x) < (y) ? (x) : (y)
 
 void swap(int *a, int *b){
     int tmp = *a;
@@ -42,24 +43,29 @@ int findRadius(int* houses, int housesSize, int* heaters, int heatersSize) {
     return 0;
   }
   int max = 0;
-  quickSort(houses, 0, houses - 1);
+  quickSort(houses, 0, housesSize - 1);
   quickSort(heaters, 0, heatersSize - 1);
   for(int i = 0; i < housesSize; i++){
     int tmp = 0;
     for(int j = 0; j < heatersSize; j++){
       if(heaters[j] == houses[i]) {
-        continue;
+        break;
       }
       if(heaters[j] < houses[i]){
         if(j == heatersSize - 1){
           tmp = houses[i] - heaters[j];
-        } else if(heater[j + 1] > houses[i]){
-          tmp = MAX(heaters[j + 1] - houses[i], houses[i] - heaters[j]);
           max = MAX(tmp, max);
-          continue;
+        } else if(heaters[j + 1] >= houses[i]){
+          max = MAX(MIN(heaters[j + 1] - houses[i],  houses[i] - heaters[j]), max);
+          break;
         }
+      } else {
+        tmp = heaters[j] - houses[i];
+        max = MAX(tmp, max);
+        break;
       }
-    }
+    }    printf("%d, %d\n", i, max);
+
   }
-  return max / 2;
+  return max;
 }
