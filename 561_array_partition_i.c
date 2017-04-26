e8eb877974392e356c9c69ec59fa2bce388dc3f0
leonard520 <xiaoydin@cisco.com>
2017-04-26 23:04:14 +0800

    easy today

---
+++ 561_array_partition_i.c
@@ -0,0 +1,46 @@
+/* Copyright (C) 2016 Leonard Ding <dingxiaoyun88@gmail.com> */
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+
+void swap(int *a, int *b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void quickSort(int* nums, int low, int high) {
+    int flag, i, j;
+    if(low < high){
+        flag = low;
+        i = low;
+        j = high;
+        while(i < j){
+            while(nums[i] <= nums[flag] && i <= high){
+                i++;
+            }
+            while(nums[j] > nums[flag] && j >= low){
+                j--;
+            }
+            if(i < j){
+                swap(&nums[i], &nums[j]);
+            }
+        }
+        swap(&nums[j], &nums[flag]);
+        quickSort(nums, low, j - 1);
+        quickSort(nums, j + 1, high);
+    }
+}
+
+int arrayPairSum(int* nums, int numsSize) {
+  if(nums == NULL) return 0;
+  int result = 0;
+  quickSort(nums, 0, numsSize - 1);
+  for(int i = 0; i < numsSize; i+=2){
+    result += nums[i];
+  }
+  return result;
+}
