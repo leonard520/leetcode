82fa27f6351a9b87c088107fc27ff7fd20492f90
leonard520 <xiaoydin@cisco.com>
2017-05-07 23:59:31 +0800

    tree

---
+++ 572_subtree_of_another_tree.c
@@ -0,0 +1,17 @@
+/* Copyright (C) 2016 Leonard Ding <dingxiaoyun88@gmail.com> */
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSubtree(struct TreeNode* s, struct TreeNode* t) {
+
+}
