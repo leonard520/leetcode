1e7431fdefec3c1f8bf16cf4a36047f65d64c89e
leonard520 <xiaoydin@cisco.com>
2017-05-08 00:11:56 +0800

    为什么对于一个很简单的题目，总是要犯各种小错误而不能一次做对呢？

--- 572_subtree_of_another_tree.c
+++ 572_subtree_of_another_tree.c
@@ -4,6 +4,16 @@
 #include <string.h>
 #include <stdio.h>
 
+bool isSubtreeInternal(struct TreeNode* s, struct TreeNode* t) {
+	if(s == NULL && t == NULL) return true;
+	if(s == NULL || t == NULL) return false;
+	if(s->val == t->val){
+		return (isSubtreeInternal(s->right, t->right) && isSubtreeInternal(s->left, t->left));
+	} else {
+		return false;
+	}
+}
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -13,5 +23,7 @@
  * };
  */
 bool isSubtree(struct TreeNode* s, struct TreeNode* t) {
-
+	if(isSubtreeInternal(s, t) == true) return true;
+	if(s == NULL || t == NULL) return false;
+	return isSubtree(s->left, t) || isSubtree(s->right, t);
 }
